42db7732ee29c9f74a107eeebb61489b4dd24e5d
bezbeli <bezbeli@gmail.com>
2017-09-06 21:13:46 +0200

    add ip restriction, make it last for 2 min

--- src/PostViews.php
+++ src/PostViews.php
@@ -20,13 +20,13 @@ class PostViews
     // function to count views.
     public function set($postID)
     {
-        $user_ip = $_SERVER['REMOTE_ADDR']; //retrieve the current IP address of the visitor
-        $key = $user_ip . 'x' . $postID; //combine post ID & IP to form unique key
-        $value = array($user_ip, $postID); // store post ID & IP as separate values (see note)
-        $visited = get_transient($key); //get transient and store in variable
+        $user_ip = $_SERVER['REMOTE_ADDR'];
+        $key = $user_ip . 'x' . $postID;
+        $value = array($user_ip, $postID);
+        $visited = get_transient($key);
 
-        if ( false === ( $visited ) ) {
-            set_transient( $key, $value, 60*2 );
+        if (false === ( $visited )) {
+            set_transient($key, $value, 60*2);
             $count_key = 'post_views_count';
             $count = get_post_meta($postID, $count_key, true);
             if ($count=='') {
