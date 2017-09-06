6264262c92bc98aecf72f9bf86dd74bc32c41399
bezbeli <bezbeli@gmail.com>
2017-09-06 19:53:48 +0200

    added PostViews class

---
+++ src/post-views.php
@@ -0,0 +1,34 @@
+<?php
+namespace Bezbeli\PostViews;
+
+class PostViews
+{
+
+    // function to display number of posts.
+    public function get($postID)
+    {
+        $count_key = 'post_views_count';
+        $count = get_post_meta($postID, $count_key, true);
+        if ($count=='') {
+            delete_post_meta($postID, $count_key);
+            add_post_meta($postID, $count_key, '0');
+            return "0 View";
+        }
+        return $count.' Views';
+    }
+
+    // function to count views.
+    public function set($postID)
+    {
+        $count_key = 'post_views_count';
+        $count = get_post_meta($postID, $count_key, true);
+        if ($count=='') {
+            $count = 0;
+            delete_post_meta($postID, $count_key);
+            add_post_meta($postID, $count_key, '0');
+        } else {
+            $count++;
+            update_post_meta($postID, $count_key, $count);
+        }
+    }
+}
